6673d24ed99c8c2e87ec8c26c6444df2be144352
ThineAcoustic <70708679+ThineAcoustic@users.noreply.github.com>
2020-12-07 10:21:02 -0500

    Running Script
    added a script with a debug to attempt to see if one portion is running.

---
+++ Tilia Splice Trainer/Assets/Running.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Running : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Workey?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
